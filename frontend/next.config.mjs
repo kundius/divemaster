f979f5499d9e7ad3a245803223ac5c5347720fe6
Kundius Ruslan <kundius.ruslan@gmail.com>
2024-03-29 02:56:11 +0300

    1

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost'
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8080'
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8080'
+      },
+    ]
+  },
   serverRuntimeConfig: {
     // secondSecret: process.env.SECRET
   },
--- frontend/src/components/admin/ProductGallery/index.tsx
+++ frontend/src/components/admin/ProductGallery/index.tsx
@@ -3,7 +3,7 @@
 import { Button } from '@/components/ui/button'
 import { Overlay } from '@/components/ui/overlay'
 import { VespTableData } from '@/components/vesp/VespTable/types'
-import { apiGet, apiPut } from '@/lib/api'
+import { apiGet, apiPost, apiPut } from '@/lib/api'
 import { withToken } from '@/lib/api/with-token'
 import { useAuth } from '@/lib/auth/use-auth'
 import { getImageLink } from '@/lib/utils'
@@ -11,11 +11,53 @@ import { VespProductFile } from '@/types'
 import { PlusCircleIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
 import { ChangeEvent, DragEvent, useEffect, useState } from 'react'
+import { ReactSortable } from 'react-sortablejs'
+import type { SortableEvent } from 'sortablejs'
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragEndEvent
+} from '@dnd-kit/core'
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy
+} from '@dnd-kit/sortable'
+import { useSortable } from '@dnd-kit/sortable'
+
+import { CSS } from '@dnd-kit/utilities'
+
+export function SortableItem(props: ItemType) {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+    id: props.id
+  })
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition
+  }
+
+  return (
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      {props.productFile.file_id}
+    </div>
+  )
+}
 
 export interface ProductGalleryProps {
   productId: number
   thumbWidth?: number
   thumbHeight?: number
+}
+
+interface ItemType {
+  id: number
+  productFile: VespProductFile
 }
 
 export function ProductGallery(props: ProductGalleryProps) {
@@ -25,7 +67,13 @@ export function ProductGallery(props: ProductGalleryProps) {
 
   const [loading, setLoading] = useState(false)
   const [total, setTotal] = useState(0)
-  const [files, setFiles] = useState<VespProductFile[]>([])
+  const [items, setItems] = useState<ItemType[]>([])
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates
+    })
+  )
 
   useEffect(() => {
     fetch()
@@ -71,8 +119,14 @@ export function ProductGallery(props: ProductGalleryProps) {
     try {
       for (const file of nativeFiles) {
         if (file.type.includes('image/')) {
-          const res = await asyncLoad(file)
-          setFiles((prev) => [...prev, res])
+          const item = await asyncLoad(file)
+          setItems((prev) => [
+            ...prev,
+            {
+              id: item.file_id,
+              productFile: item
+            }
+          ])
         }
       }
     } finally {
@@ -84,14 +138,60 @@ export function ProductGallery(props: ProductGalleryProps) {
     setLoading(true)
     try {
       const data = await apiGet<VespTableData<VespProductFile>>(url, {}, withToken(auth.token)())
-      setFiles(data.rows)
+      setItems(
+        data.rows.map((item) => ({
+          id: item.file_id,
+          productFile: item
+        }))
+      )
       setTotal(data.total)
     } finally {
       setLoading(false)
     }
   }
 
-  console.log(files)
+  // function setList(newList: ItemType[]) {
+  //   setItems(newList)
+  //   const _files: { [key: number]: number } = {}
+  //   newList.forEach((i, idx) => {
+  //     _files[i.file_id] = idx
+  //   })
+  //   apiPost(url, { files: _files })
+  // }
+
+  // function onSort() {
+  //   setTimeout(async () => {
+  //     // setItems(newList)
+  //     const _files: { [key: number]: number } = {}
+  //     items.forEach((i, idx) => {
+  //       _files[i.file_id] = idx
+  //     })
+  //     await apiPost(url, { files: _files })
+  //   }, 0)
+  // }
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event
+
+    let newArray: ItemType[] = []
+
+    if (active.id !== over?.id) {
+      setItems((items) => {
+        const oldIndex = items.findIndex((item) => item.id === active.id)
+        const newIndex = items.findIndex((item) => item.id === over?.id)
+
+        newArray = arrayMove(items, oldIndex, newIndex)
+
+        return newArray
+      })
+
+      const ordered: { [key: number]: number } = {}
+      newArray.forEach((item, idx) => {
+        ordered[item.id] = idx
+      })
+      apiPost(url, { files: ordered })
+    }
+  }
 
   return (
     <div>
@@ -112,21 +212,35 @@ export function ProductGallery(props: ProductGalleryProps) {
             e.preventDefault()
           }}
         >
-          {files.map((item) => (
-            <div key={item.file_id} className='border rounded-lg p-2 hover:border-neutral-300 hover:bg-neutral-50 hover:shadow-sm card'>
-              <Image
-                src={getImageLink(item.file, { fit: 'crop', w: thumbWidth, h: thumbHeight })}
-                width={thumbWidth}
-                height={thumbHeight}
-                alt="Picture of the author"
-              />
-              <div className='flex gap-2 justify-end mt-2'>
-                <Button>
+          {/* className="grid grid-cols-4 gap-4" */}
 
-                </Button>
-              </div>
-            </div>
-          ))}
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
+          >
+            <SortableContext items={items} strategy={verticalListSortingStrategy}>
+              {items.map((item) => (
+                <SortableItem key={item.id} {...item} />
+              ))}
+              {/* {items.map((item) => (
+                <div
+                  key={item.id}
+                  className="border rounded-lg p-2 hover:border-neutral-300 hover:bg-neutral-50 hover:shadow-sm card"
+                >
+                  <Image
+                    src={getImageLink(item.file, { fit: 'crop', w: thumbWidth, h: thumbHeight })}
+                    width={thumbWidth}
+                    height={thumbHeight}
+                    alt="Picture of the author"
+                  />
+                  <div className="flex gap-2 justify-end mt-2">
+                    <Button></Button>
+                  </div>
+                </div>
+              ))} */}
+            </SortableContext>
+          </DndContext>
         </div>
       </Overlay>
     </div>

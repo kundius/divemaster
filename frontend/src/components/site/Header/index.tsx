02ecfa44ba07ba4cc0c55afcf40085c40d00ee96
Kundius Ruslan <kundius.ruslan@gmail.com>
2024-04-04 17:59:22 +0300

    1

--- frontend/src/components/site/Header/index.tsx
+++ frontend/src/components/site/Header/index.tsx
@@ -19,83 +19,85 @@ export function Header() {
   return (
     <>
       <div className={styles.top}>
-        <Container className={styles['top-container']}>
-          <div className={styles['mobile-contacts']}>
-            <MobileContacts />
-          </div>
+        <Container>
+          <div className={styles['top-container']}>
+            <div className={styles['mobile-contacts']}>
+              <MobileContacts />
+            </div>
 
-          <div className={styles['top-menu-primary']}>
-            <TopMenu
-              primary={[
-                {
-                  title: 'Доставка и оплата',
-                  href: '/',
-                  icon: 'delivery'
-                },
-                {
-                  title: 'Оптовикам',
-                  href: '#',
-                  icon: 'salers'
-                },
-                {
-                  title: 'Контакты',
-                  href: '/contacts',
-                  icon: 'contacts'
-                },
-                {
-                  title: 'Блог',
-                  href: '#',
-                  icon: 'blog',
-                  className: 'max-lg:hidden'
-                }
-              ]}
-              secondary={[
-                {
-                  title: 'Блог',
-                  href: '#',
-                  className: 'lg:hidden'
-                },
-                {
-                  title: 'О магазине',
-                  href: '/'
-                },
-                {
-                  title: 'Скидочные карты',
-                  href: '/contacts'
-                },
-                {
-                  title: 'Подарочные сертификаты',
-                  href: '#',
-                  className: '2xl:hidden'
-                },
-                {
-                  title: 'Школа подводной охоты и дайвинга',
-                  href: '#',
-                  className: '2xl:hidden'
-                }
-              ]}
-            />
-          </div>
+            <div className={styles['top-menu-primary']}>
+              <TopMenu
+                primary={[
+                  {
+                    title: 'Доставка и оплата',
+                    href: '/',
+                    icon: 'delivery'
+                  },
+                  {
+                    title: 'Оптовикам',
+                    href: '#',
+                    icon: 'salers'
+                  },
+                  {
+                    title: 'Контакты',
+                    href: '/contacts',
+                    icon: 'contacts'
+                  },
+                  {
+                    title: 'Блог',
+                    href: '#',
+                    icon: 'blog',
+                    className: 'max-lg:hidden'
+                  }
+                ]}
+                secondary={[
+                  {
+                    title: 'Блог',
+                    href: '#',
+                    className: 'lg:hidden'
+                  },
+                  {
+                    title: 'О магазине',
+                    href: '/'
+                  },
+                  {
+                    title: 'Скидочные карты',
+                    href: '/contacts'
+                  },
+                  {
+                    title: 'Подарочные сертификаты',
+                    href: '#',
+                    className: '2xl:hidden'
+                  },
+                  {
+                    title: 'Школа подводной охоты и дайвинга',
+                    href: '#',
+                    className: '2xl:hidden'
+                  }
+                ]}
+              />
+            </div>
 
-          <div className={styles['top-menu-secondary']}>
-            <TopMenu
-              primary={[
-                {
-                  title: 'Подарочные сертификаты',
-                  href: '#',
-                  icon: 'gift'
-                },
-                {
-                  title: 'Школа подводной охоты и дайвинга',
-                  href: '#',
-                  icon: 'school'
-                }
-              ]}
-            />
-          </div>
+            <div className={styles['top-menu-secondary']}>
+              <TopMenu
+                primary={[
+                  {
+                    title: 'Подарочные сертификаты',
+                    href: '#',
+                    icon: 'gift'
+                  },
+                  {
+                    title: 'Школа подводной охоты и дайвинга',
+                    href: '#',
+                    icon: 'school'
+                  }
+                ]}
+              />
+            </div>
 
-          <div className={styles['city-select']}>
-            <CitySelect />
+            <div className={styles['city-select']}>
+              <CitySelect />
+            </div>
           </div>
         </Container>
       </div>
@@ -104,8 +106,8 @@ export function Header() {
           <div className={styles['primary-container']}>
             <div className={styles.logo}>
               <Link href="/">
-                <Image src="/logo.png" width={148} height={71} alt="" className='max-md:hidden' />
-                <Image src="/small-logo.png" width={43} height={48} alt="" className='md:hidden' />
+                <Image src="/logo.png" width={148} height={71} alt="" className="max-md:hidden" />
+                <Image src="/small-logo.png" width={43} height={48} alt="" className="md:hidden" />
               </Link>
             </div>
 

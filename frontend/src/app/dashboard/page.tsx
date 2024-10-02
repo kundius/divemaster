00ed60c13795ee4229956111257c57566380f701
Kundius Ruslan <kundius.ruslan@yandex.ru>
2024-10-02 16:17:51 +0300

    123

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -2,7 +2,10 @@ import { DashboardPage } from '@/components/admin/DashboardPage'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Панель управления'
+  title: 'Панель управления',
+  description:
+    'Интернет-магазин подводного снаряжения ДАЙВМАСТЕР. Всё для подводной охоты, дайвинга, плавания - DiveMaster. Предлагаем купить на сайте экипировку оптом и в розницу. Низкие цены в Воронеже, Москве, СПб, Краснодаре, России и других странах мира. Бесплатная доставка.',
+  keywords: 'подводное снаряжение, подводная охота, дайвинг, плавание'
 }
 
 export default function Page() {

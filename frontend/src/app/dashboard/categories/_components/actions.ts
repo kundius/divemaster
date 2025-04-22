7ee0df1a4d197bb2031a3cec0f7f7a0fdac6610e
Kundius Ruslan <kundius.ruslan@yandex.ru>
2025-04-22 03:26:45 +0300

    123

--- frontend/src/app/dashboard/categories/_components/actions.ts
+++ frontend/src/app/dashboard/categories/_components/actions.ts
@@ -5,5 +5,5 @@ import { revalidatePath } from 'next/cache'
 export async function revalidateCategory(alias: string) {
   console.log('revalidating category', `/category/${alias}`)
   // revalidatePath(`/category/${alias}`, 'page')
-  revalidatePath('/category/[alias]', 'page')
+  revalidatePath('/category/[alias]', 'layout')
 }

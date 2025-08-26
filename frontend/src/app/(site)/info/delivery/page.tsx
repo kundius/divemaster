27aa05c9ae1ec27a3b5136f533442b4636eca058
Kundius Ruslan <kundius.ruslan@yandex.ru>
2025-08-26 21:23:44 +0300

    123

--- frontend/src/app/(site)/info/delivery/page.tsx
+++ frontend/src/app/(site)/info/delivery/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     <SectionPage>
       <Headline className="mb-12 max-lg:mb-8 max-md:mb-6" title="Доставка" />
       <div className="mx-auto prose lg:prose-xl">
-        <h2>Доставка ТК "СДЭК"</h2>
+        <h2>Доставка ТК &ldquo;СДЭК&rdquo;</h2>
       </div>
     </SectionPage>
   )

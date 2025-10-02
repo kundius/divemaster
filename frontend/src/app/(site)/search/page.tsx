e70b48565dc1d2c12f4dc1882500ee9ee15d288a
Kundius Ruslan <kundius.ruslan@yandex.ru>
2025-10-02 14:38:47 +0300

    123

--- frontend/src/app/(site)/search/page.tsx
+++ frontend/src/app/(site)/search/page.tsx
@@ -8,6 +8,7 @@ import { ProductsList } from '../_components/ProductsList'
 import { ProductsPagination } from '../_components/ProductsPagination'
 import { ProductsSorting } from '../_components/ProductsSorting'
 import { SearchHeadline } from './_components/SearchHeadline'
+import { Suspense } from 'react'
 
 export const metadata: Metadata = {
   title: 'Поиск'
@@ -16,41 +17,43 @@ export const metadata: Metadata = {
 export default async function Page() {
   return (
     <SectionPage>
-      <ProductsStoreProvider>
-        <SearchHeadline />
+      <Suspense>
+        <ProductsStoreProvider>
+          <SearchHeadline />
 
-        <div className="flex gap-x-5 mt-14">
-          <div className="w-[320px] max-xl:w-[260px] flex-shrink-0 space-y-5 max-lg:hidden">
-            <div className="mb-80">
-              <ProductsFilter />
+          <div className="flex gap-x-5 mt-14">
+            <div className="w-[320px] max-xl:w-[260px] flex-shrink-0 space-y-5 max-lg:hidden">
+              <div className="mb-80">
+                <ProductsFilter />
+              </div>
+              <ConsultationWidget />
+              <BenefitsSideSlider
+                items={[
+                  {
+                    content: <BenefitsSideSliderDiscount />,
+                    name: 'BenefitsSideSliderDiscount1'
+                  },
+                  {
+                    content: <BenefitsSideSliderDiscount />,
+                    name: 'BenefitsSideSliderDiscount2'
+                  },
+                  {
+                    content: <BenefitsSideSliderDiscount />,
+                    name: 'BenefitsSideSliderDiscount3'
+                  }
+                ]}
+              />
             </div>
-            <ConsultationWidget />
-            <BenefitsSideSlider
-              items={[
-                {
-                  content: <BenefitsSideSliderDiscount />,
-                  name: 'BenefitsSideSliderDiscount1'
-                },
-                {
-                  content: <BenefitsSideSliderDiscount />,
-                  name: 'BenefitsSideSliderDiscount2'
-                },
-                {
-                  content: <BenefitsSideSliderDiscount />,
-                  name: 'BenefitsSideSliderDiscount3'
-                }
-              ]}
-            />
-          </div>
-          <div className="w-full">
-            <div className="hidden">
-              <ProductsSorting />
+            <div className="w-full">
+              <div className="hidden">
+                <ProductsSorting />
+              </div>
+              <ProductsList />
+              <ProductsPagination />
             </div>
-            <ProductsList />
-            <ProductsPagination />
           </div>
-        </div>
-      </ProductsStoreProvider>
+        </ProductsStoreProvider>
+      </Suspense>
     </SectionPage>
   )
 }
